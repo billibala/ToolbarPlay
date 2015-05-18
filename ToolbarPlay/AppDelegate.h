f79b0e80b92c8c6f1241ac3e166fe4afeea05f11
Bill So <hokchun@gmail.com>
2015-05-18 11:04:21 +0800

    Initial Commit

---
+++ ToolbarPlay/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToolbarPlay
+//
+//  Created by Bill So on 5/18/15.
+//  Copyright (c) 2015 Bill So. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ToolbarPlay/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ToolbarPlay
+//
+//  Created by Bill So on 5/18/15.
+//  Copyright (c) 2015 Bill So. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
